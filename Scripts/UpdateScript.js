ee2f5f0b3990ac8dd7a6494faa40f440f21b19c6
Ali Faris <alifaris@earthlink.iq>
2021-11-06 22:07:16 +0300

    update script

--- Scripts/UpdateScript.js
+++ Scripts/UpdateScript.js
@@ -8,27 +8,26 @@ Steps:
 
 const spawn = require('child_process').spawn;
 
-
-function buildBackendApp() {
-    const backendProcess = spawn('npm.cmd', ['run' , 'build']);
-    printData(backendProcess, "BackendApp");
-    backendProcess.on('close', function (code) {
-        console.log('Backend App Build Finished, Status = ' + code);
+function updateFromGit() {
+    spawn('git', ['checkout' , '.']);
+    spawn('git', ['checkout' , 'master']);
+    const gitPullProcess = spawn('git', ['pull']);
+    printData(gitPullProcess, "Update Code");
+    gitPullProcess.on('close', function (code) {
+        console.log('Update Code Finished, Status = ' + code);
     });
 }
 
-function gitPull() {
-    const frontendProcess = spawn('npm.cmd', ['run' , 'build']);
-    printData(frontendProcess, "FrontendServer");
-    frontendProcess.on('close', function (code) {
-        console.log('Frontend App Build Finished, Status = ' + code);
+function printData(process, processName) {
+    process.stdout.setEncoding('utf8');
+    process.stdout.on('data', function (data) {
+        const str = data.toString()
+        const lines = [`=================${processName}=================`, str.split(/(\r?\n)/g), `=================${processName}=================`];
+        console.log(lines.join(""));
     });
 }
 
 
-console.log("Building COVID Result App");
+console.log("Updating Code: COVID Result App");
 console.log("===================");
-process.chdir('Backend');
-buildBackendApp();
-process.chdir("../frontend");
-buildFrontendApp();
+updateFromGit();
