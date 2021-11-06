e59985f25fea1c58e8feabd175b0d2f8b3c09995
Ali Faris <alifaris@earthlink.iq>
2021-11-06 21:49:02 +0300

    run script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prerequisite Software
+
+- NodeJS
+- Git
+
---
+++ RunScript.js
@@ -0,0 +1,43 @@
+/*
+Steps:
+
+- Run Backend Server
+- Run Frontend Server
+
+*/
+
+const spawn = require('child_process').spawn;
+
+
+function runBackendServer() {
+    const backendProcess = spawn('node', ['dist/main']);
+    printData(backendProcess, "BackendServer");
+    backendProcess.on('close', function (code) {
+        console.log('Backend Server Closed ' + code);
+    });
+}
+
+function runFrontendServer() {
+    const frontendProcess = spawn('npm.cmd', ['run' , 'start'] , {cwd : process.cwd()});
+    printData(frontendProcess, "FrontendServer");
+    frontendProcess.on('close', function (code) {
+        console.log('Frontend Server Closed ' + code);
+    });
+}
+
+function printData(process, processName) {
+    process.stdout.setEncoding('utf8');
+    process.stdout.on('data', function (data) {
+        const str = data.toString()
+        const lines = [`=================${processName}=================`, str.split(/(\r?\n)/g), `=================${processName}=================`];
+        console.log(lines.join(""));
+    });
+}
+
+
+console.log("Booting COVID Result App");
+console.log("===================");
+process.chdir('backend');
+runBackendServer();
+process.chdir("../frontend");
+runFrontendServer();
